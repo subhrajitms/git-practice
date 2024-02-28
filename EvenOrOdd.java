fa0d516f7652fe6392ba8f7781b142fc0dba9a85
subhrajitms <subhrajitsahoo186@gmail.com>
2024-02-28 12:19:55 +0530

    added

---
+++ EvenOrOdd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class EvenOrOdd{
+   public static void main(String[] args)
+	{
+	   Scanner ip=new Scanner(System.in);
+	   int num=ip.nextInt();
+	   if(num%2==0)
+	   System.out.println(num+" is a even number ");
+	   else
+	   System.out.println(num + is a odd number ");
+	}
+}
